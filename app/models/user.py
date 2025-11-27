336847069a710718684912ec2c430c425e0e26c7
kangchan <dodorami213@gmail.com>
2025-11-27 23:32:41 +0900

    수정1

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
-from app.core.database import Base
+from app.core.database import Base 
 from datetime import datetime
 from .allergy import user_allergy_association
 
