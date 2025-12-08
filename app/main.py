9e108507cbc717486971412339eebfbb729e377d
Seungwon12345 <lesw04100@naver.com>
2025-12-08 15:10:50 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,6 +6,7 @@ from fastapi.openapi.utils import get_openapi
 from app.api.endpoints import main_page as main_page_api
 from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import meals as meals_api
+from app.api.endpoints import exercise
 
 app = FastAPI(
     title="CareView API",
@@ -32,6 +33,7 @@ app.include_router(onboarding_api.router, tags=["Onboarding"])
 app.include_router(record_api.router, tags=["Record"])
 app.include_router(main_page_api.router, tags=["Main Page"])
 app.include_router(meals_api.router, tags=["meals"])
+app.include_router(exercise.router, tags=["Exercise"])
 
 @app.get("/")
 def read_root():
@@ -62,7 +64,7 @@ def custom_openapi():
         for method in route.values():
             tags = method.get('tags', [])
             # 🚨 수정: 인증이 필요한 모든 태그에 보안 적용
-            if tags and any(tag in ['Users', 'Onboarding', 'Record', 'Main Page', 'meals'] for tag in tags):
+            if tags and any(tag in ['Users', 'Onboarding', 'Record', 'Main Page', 'meals', 'Exercise'] for tag in tags):
                 method["security"] = security_requirement
 
     app.openapi_schema = openapi_schema
