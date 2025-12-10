b2f333b7deae6e61d5896f919b4bb40c2356f76a
kangchan <dodorami213@gmail.com>
2025-12-10 21:28:52 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -19,6 +19,7 @@ origins = [
     "http://localhost:3000",  
     "http://localhost:8000",
     "http://localhost:5173",
+    "https://careview-frontend.onrender.com"
 ]
 # 3. 미들웨어 추가
 app.add_middleware(
