a031833030740714e68ea038101d64f648eacac1
Kangchan Seo <dodorami213@gmail.com>
2026-08-13 17:02:09 +0900

    Update security.py

--- app/core/security.py
+++ app/core/security.py
@@ -10,12 +10,9 @@ from app.core.config import settings
 pwd_context = CryptContext(schemes=["sha256_crypt"], deprecated="auto")
 
 
-# 비밀번호 검증 (로그인 시 사용)
 def verify_password(plain_password: str, hashed_password: str) -> bool:
-    # 일반 텍스트 비밀번호와 해시된 비밀번호를 비교
     return pwd_context.verify(plain_password, hashed_password)
 
-# JWT 토큰 생성
 def create_access_token(subject: str | Any, expires_delta: timedelta | None = None) -> str:
 
     if expires_delta:
@@ -23,14 +20,11 @@ def create_access_token(subject: str | Any, expires_delta: timedelta | None = No
     else:
         expire = datetime.now(timezone.utc) + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
     
-    # 토큰에 포함될 데이터 (payload)
     to_encode = {"exp": expire, "sub": str(subject)}
     
-    # JWT 인코딩
     encoded_jwt = jwt.encode(to_encode, settings.SECRET_KEY, algorithm=settings.ALGORITHM)
     return encoded_jwt
 
-# 3. 토큰 디코딩
 def decode_token(token: str) -> dict[str, Any] | None:
     try:
         payload = jwt.decode(
