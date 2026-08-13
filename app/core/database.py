895f00720469712ecb2738b708a6337c41fa9a05
Kangchan Seo <dodorami213@gmail.com>
2026-08-13 17:01:20 +0900

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -18,10 +18,8 @@ SessionLocal = sessionmaker(
     expire_on_commit=False
 )
 
-# 모든 모델이 상속받을 기본 클래스
 Base = declarative_base()
 
-# FastAPI 의존성 주입을 위한 DB 세션 함수
 def get_db():
     db = SessionLocal()
     try:
